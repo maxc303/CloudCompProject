6dc87cdba06c66c6c297a129877b80815f762547
Minfeng-Xu <minfeng.xu1119@gmail.com>
2020-04-01 19:04:34 -0400

    add games into database

--- app/tools/web_spider.py
+++ app/tools/web_spider.py
@@ -1,15 +1,29 @@
 import requests
-import json
+import re
+import app.utils as u
+from bs4 import BeautifulSoup
 if __name__ == '__main__':
-    target = 'https://en.wikipedia.org/wiki/List_of_PlayStation_4_games#0%E2%80%939'
-    req = requests.get(url=target)
-    file = '../spider.json'
+    file = open('ps4_games.html','r',encoding='utf-8')
+    soup = BeautifulSoup(file, 'html.parser')
+    text = soup.find_all('tr',)
+    for tr in text:
 
-    fo = open("test.txt", "w")
-    print
-    "文件名为: ", fo.name
-    str = "菜鸟教程"
-    fo.write(str)
+        print('-'*100)
+        td = tr.find_all('td')
+        if(td[0].find('a')):
+            if not(td[1].find('a')):
+                url = re.findall(r'href="(.+?)"', str(td[0]))
+                name = re.findall(r'">(.+?)</a>', str(td[0]))
+                date = re.findall(r'">(.+?)</span>', str(td[6]))
+                if(td[1].find('li')):
+                    li = td[1].find('li')
+                    genre = re.findall(r'<li>(.+?)</li>', str(li))
+                else:
+                    genre = re.findall(r'<td>(.+?)</td>', str(td[1]), re.S)
+                print("url",str(url))
+                print("name",str(name))
+                print('genre',genre)
+                print('date',date)
+                u.put_item(str(name), str(genre),str(url))
 
-    # 关闭文件
-    fo.close()
+
