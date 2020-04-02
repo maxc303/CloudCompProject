87c9c09d6813904db8643949697f2adf801f9358
Minfeng-Xu <minfeng.xu1119@gmail.com>
2020-04-02 19:30:31 -0400

    fix some problem in tools

--- app/tools/web_spider.py
+++ app/tools/web_spider.py
@@ -15,15 +15,25 @@ if __name__ == '__main__':
                 url = re.findall(r'href="(.+?)"', str(td[0]))
                 url = str(url).split("'")[1]
                 name = re.findall(r'">(.+?)</a>', str(td[0]))
-                name = "'".join(str(name).split("'")[1:-1])
+                if(str(name).__contains__('"')):
+                    name = str(name).split('"')[1]
+                else:
+                    name = str(name).split("'")[1]
                 if(td[1].find('li')):
                     li = td[1].find('li')
                     genre = re.findall(r'<li>(.+?)</li>', str(li))
-                    genre = str(genre).split("'")[1]
+                    if(str(genre).__contains__('"')):
+                        genre = str(genre).split('"')[1]
+                    else:
+                        genre = str(genre).split("'")[1]
                     genre = genre.split("\\n")[0]
                 else:
                     genre = re.findall(r'<td>(.+?)</td>', str(td[1]), re.S)
-                    genre = str(genre).split("'")[1]
+                    if (str(genre).__contains__('"')):
+                        genre = str(genre).split('"')[1]
+                    else:
+                        genre = str(genre).split("'")[1]
+                    genre = genre.split("\\n")[0]
                     genre = genre.split("\\n")[0]
                 print("url",str(url))
                 print("name",str(name))
